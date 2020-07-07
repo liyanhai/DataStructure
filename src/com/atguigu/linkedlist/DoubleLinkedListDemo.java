77b4b565c9ea88ac7e04c55569a9c3897e380e6d
李艳海 <liyanhai@koolearn.com>
2020-07-07 10:11:28 +0800

    双向链表-按照顺序添加节点

--- src/com/atguigu/linkedlist/DoubleLinkedListDemo.java
+++ src/com/atguigu/linkedlist/DoubleLinkedListDemo.java
@@ -6,29 +6,33 @@ public class DoubleLinkedListDemo {
 		// 测试
 		System.out.println("双向链表的测试");
 		// 先创建节点
-		HeroNode2 hero1 = new HeroNode2(1, "宋江", "及时雨");
-		HeroNode2 hero2 = new HeroNode2(2, "卢俊义", "玉麒麟");
-		HeroNode2 hero3 = new HeroNode2(3, "吴用", "智多星");
-		HeroNode2 hero4 = new HeroNode2(4, "林冲", "豹子头");
+		HeroNode2 hero1 = new HeroNode2(3, "宋江", "及时雨");
+		HeroNode2 hero2 = new HeroNode2(1, "卢俊义", "玉麒麟");
+		HeroNode2 hero3 = new HeroNode2(4, "吴用", "智多星");
+		HeroNode2 hero4 = new HeroNode2(2, "林冲", "豹子头");
+
+
+
+
 		// 创建一个双向链表
 		DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
-		doubleLinkedList.add(hero1);
-		doubleLinkedList.add(hero2);
-		doubleLinkedList.add(hero3);
-		doubleLinkedList.add(hero4);
-		
+		doubleLinkedList.addByOrder(hero1);
+		doubleLinkedList.addByOrder(hero2);
+		doubleLinkedList.addByOrder(hero3);
+		doubleLinkedList.addByOrder(hero4);
+//
 		doubleLinkedList.list();
-		
-		// 修改
-		HeroNode2 newHeroNode = new HeroNode2(4, "公孙胜", "入云龙");
-		doubleLinkedList.update(newHeroNode);
-		System.out.println("修改后的链表情况");
-		doubleLinkedList.list();
-		
-		// 删除
-		doubleLinkedList.del(3);
-		System.out.println("删除后的链表情况~~");
-		doubleLinkedList.list();
+//
+//		// 修改
+//		HeroNode2 newHeroNode = new HeroNode2(4, "公孙胜", "入云龙");
+//		doubleLinkedList.update(newHeroNode);
+//		System.out.println("修改后的链表情况");
+//		doubleLinkedList.list();
+//
+//		// 删除
+//		doubleLinkedList.del(3);
+//		System.out.println("删除后的链表情况~~");
+//		doubleLinkedList.list();
 		
 		
 		
@@ -87,6 +91,35 @@ class DoubleLinkedList {
 		// 形成一个双向链表
 		temp.next = heroNode;
 		heroNode.pre = temp;
+	}
+
+	//根据编号添加节点
+	public void addByOrder(HeroNode2 heroNode){
+		HeroNode2 temp = head;
+		boolean flag = false; // flag标志添加的编号是否存在，默认为false
+		while (true){
+			if(temp.next == null){
+				break;
+			}
+			if(temp.next.no > heroNode.no){
+				break;
+			}else if(temp.next.no == heroNode.no){
+				flag = true;
+				break;
+			}
+			temp = temp.next;
+		}
+
+		if(flag){
+			System.out.printf("准备插入的英雄的编号 %d 已经存在了, 不能加入\n", heroNode.no);
+		}else{
+			if(temp.next != null){
+				heroNode.next = temp.next;
+				temp.next.pre = heroNode;
+			}
+			temp.next = heroNode;
+			heroNode.pre = temp;
+		}
 	}
 
 	// 修改一个节点的内容, 可以看到双向链表的节点内容修改和单向链表一样
